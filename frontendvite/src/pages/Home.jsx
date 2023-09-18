226c96f2b76361f45c376f87e1c62fbf8867ada7
avd <avd-web@outlook.com>
2023-09-18 15:20:01 +0200

    Adding-Userpage

--- frontendvite/src/pages/Home.jsx
+++ frontendvite/src/pages/Home.jsx
@@ -1,15 +1,29 @@
 import React from "react";
 import Register from "./Register";
 import Login from "./Login";
+import { useState } from "react";
 
 export default function HomePage() {
+  const [color, setColor] = useState("red");
+  const [user, setUser] = useState("");
+
   return (
-    <div>
-      <div>
-        <h1> Home </h1>
-        <Register />
-        <Login />
-      </div>
-    </div>
+    <>
+      <Login />
+      <Register />
+
+      <h1>My favorite color is {color}!</h1>
+      <button type="button" onClick={() => setColor("blue")}>
+        Blue
+      </button>
+
+      <h1>My name is {user}!</h1>
+      <button
+        type="button"
+        onClick={() => setUser(sessionStorage.getItem("key"))}
+      >
+        show name
+      </button>
+    </>
   );
 }
