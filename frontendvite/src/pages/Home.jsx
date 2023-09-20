c107927a9e27683d9a025fcd24fac846393538ce
avd <avd-web@outlook.com>
2023-09-20 16:02:05 +0200

    working on useContext

--- frontendvite/src/pages/Home.jsx
+++ frontendvite/src/pages/Home.jsx
@@ -1,38 +1,45 @@
 import React from "react";
-import { useState } from "react";
-import LoginPage from "./LoginPage";
 import axios from "axios";
 import { AuthHeader } from "../auth/authorization";
-import Auth from "./Auth";
+import { DashboardContext } from "./Context";
+import UserPage from "./UserPage";
 
 export default function HomePage() {
-  // const [show, setShow] = useState(true);
-
-  //WRITE FUNCTION HERE
-  //I WANT TO sessionStorage.getItem("user_email") ON LOADING HOMEPAGE
-
-  // let token = sessionStorage.getItem("key");
-  // console.log(token);
-
-  // console.log(AuthHeader());
-
-  // axios
-  //   .get("http://localhost:8080/api/v1/demo-controller", AuthHeader())
-  //   .then((response) => console.log(response));
-
-  // axios.get(
-  //   "http://localhost:8080/api/v1/demo-controller",
-  //   sessionStorage.getItem("key")
-  // );
+  const userDetails = axios
+    .get("http://localhost:8080/api/v1/user", AuthHeader())
+    .then((response) => {
+      const UserDetails = {
+        userFirstName: response.data.firstname,
+        userLastName: response.data.lastname,
+        userEmail: response.data.email,
+      };
+      return userDetails;
+    })
+    .catch((error) => console.log(error));
 
   return (
     <>
-      <div>
-        <Auth />
-        {/* <LoginPage /> */}
-        {/* <button onClick={() => setShow(!show)}> show </button>
-        {show ? null : <LoginPage />} */}
-      </div>
+      <DashboardContext.Provider value={userDetails}>
+        <UserPage />
+      </DashboardContext.Provider>
     </>
   );
+
+  // return <UserInfo userName={result.userFirstName} />;
 }
+
+// function UserInfo({ userName }) {
+//   return <span>{userName}</span>;
+// }
+
+// return (
+//   <>
+//     <div>
+//       <UserInfo />
+//       {/* <Auth /> */}
+//       {/* <LoginPage /> */}
+//       {/* <button onClick={() => setShow(!show)}> show </button>
+//         {show ? null : <LoginPage />} */}
+//     </div>
+//   </>
+// );
--- frontendvite/src/pages/UserPage.jsx
+++ frontendvite/src/pages/UserPage.jsx
@@ -1,25 +1,27 @@
-import React from "react";
+import React, { useContext } from "react";
 import axios from "axios";
-import { AuthHeader } from "../auth/authorization";
+// import { AuthHeader } from "../auth/authorization";
+import { DashboardContext } from "./Context";
 
 export default function UserPage() {
+  const user = useContext(DashboardContext);
   // function getUser sessionStorage.getItem("user_email")
 
-  function getUser() {
-    axios
-      .get("http://localhost:8080/api/v1/user", AuthHeader())
-      .then((response) => {
-        const currentUser = {
-          firstname: response.data.firstname,
-          lastname: response.data.lastname,
-          email: response.data.email,
-          //ADD MORE
-        };
-        console.log(currentUser);
-      })
-      .catch();
-    //ADD CATCH
-  }
+  // function getUser() {
+  //   axios
+  //     .get("http://localhost:8080/api/v1/user", AuthHeader())
+  //     .then((response) => {
+  //       const currentUser = {
+  //         firstname: response.data.firstname,
+  //         lastname: response.data.lastname,
+  //         email: response.data.email,
+  //         //ADD MORE
+  //       };
+  //       console.log(currentUser);
+  //     })
+  //     .catch();
+  //   //ADD CATCH
+  // }
 
-  return <>{getUser()}</>;
+  return <></>;
 }
